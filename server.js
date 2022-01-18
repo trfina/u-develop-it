00e73a838e9c7172e1c53b8250d5bf82574ee4f5
Tammy Fina <finatammy@gmail.com>
2022-01-18 15:46:19 -0500

    server.js api endpoint updated to include JOIN in sql command

--- server.js
+++ server.js
@@ -33,7 +33,11 @@ const db = mysql.createConnection(
 //SELECT all candidates 
 // db object is using the SQL SELECT query() method and executes the callback with all the resulting rows that match the query.
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -50,7 +54,13 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidates/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
+                
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
