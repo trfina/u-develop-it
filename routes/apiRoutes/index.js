17ebf3e3ad4f1c9a6963df6ae768e5d14cee3b37
Tammy Fina <finatammy@gmail.com>
2022-01-19 13:11:56 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 // import routes for each api endpoint
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
